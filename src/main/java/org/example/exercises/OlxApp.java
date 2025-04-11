dcdc9b180b3ee6587843e586aaffb483ba9969b2
shreyaOLX <shreya.shukla@olx.in>
2025-04-11 19:20:23 +0530

    change in code for clean code

--- src/main/java/org/example/exercises/OlxApp.java
+++ src/main/java/org/example/exercises/OlxApp.java
@@ -3,91 +3,89 @@ package org.example.exercises;
 import java.util.Arrays;
 
 public class OlxApp {
-	
-	public Item[] items;
 
-	public OlxApp(Item[] items) {
-		this.items = items;
-	}
+    public Item[] items;
 
-	public static void main(String[] args) {
+    public OlxApp(Item[] items) {
+        this.items = items;
+    }
 
-		Item[] items = new Item[] { 
-							new Item("Mangoes", 10, 20)
-					   };
+    public static void main(String[] args) {
+        Item[] items = new Item[]{
+                new Item("Mangoes", 10, 20)
+        };
 
-		OlxApp app = new OlxApp(items);
+        OlxApp app = new OlxApp(items);
+        app.updateQuality();
 
-		app.updateQuality();
+        System.out.println(app);
+    }
 
-		System.out.println(app);
+    public void updateQuality() {
+        for (Item item : items) {
+            if (!isLegendary(item)) {
+                updateItem(item);
+                decreaseSellIn(item);
+                handleExpiredItem(item);
+            }
+        }
+    }
 
-	}
+    private void updateItem(Item item) {
+        if (isAgedItem(item)) {
+            increaseQuality(item);
 
-	public void updateQuality() {
-		for (int i = 0; i < items.length; i++) {
-			if (items[i].name != "Aged Wine" && items[i].name != "ColdPlay concert passes") {
-				if (items[i].quality > 0) {
-					if (items[i].name != "PokemonGo") {
-						items[i].quality = items[i].quality - 1;
-					}
-				}
-			} else {
-				// This part handles the items for which quality can increase
-				// "ColdPlay concert passes" and "Aged Wine"
+            if (isBackstagePass(item)) {
+                if (item.sellIn < 11) increaseQuality(item);
+                if (item.sellIn < 6) increaseQuality(item);
+            }
+        } else {
+            decreaseQuality(item);
+        }
+    }
 
-				if (items[i].quality < 50) {
-					items[i].quality = items[i].quality + 1;
+    private void handleExpiredItem(Item item) {
+        if (item.sellIn < 0) {
+            if (isAgedItem(item)) {
+                increaseQuality(item);
+            } else if (isBackstagePass(item)) {
+                item.quality = 0;
+            } else {
+                decreaseQuality(item);
+            }
+        }
+    }
 
-					if (items[i].name == "ColdPlay concert passes") {
-						if (items[i].sellIn < 11) {
-							if (items[i].quality < 50) {
-								items[i].quality = items[i].quality + 1;
-							}
-						}
+    private boolean isAgedItem(Item item) {
+        return item.name.equals("Aged Wine") || isBackstagePass(item);
+    }
 
-						if (items[i].sellIn < 6) {
-							if (items[i].quality < 50) {
-								items[i].quality = items[i].quality + 1;
-							}
-						}
-					}
-				}
-			}
+    private boolean isBackstagePass(Item item) {
+        return item.name.equals("ColdPlay concert passes");
+    }
 
-			// Everything except for Sulfuras the sellIn Decreases
-			if (items[i].name != "PokemonGo") {
-				items[i].sellIn = items[i].sellIn - 1;
-			}
+    private boolean isLegendary(Item item) {
+        return item.name.equals("PokemonGo");
+    }
 
-			if (items[i].sellIn < 0) {
-				if (items[i].name != "Aged Wine") {
+    private void increaseQuality(Item item) {
+        if (item.quality < 50) {
+            item.quality++;
+        }
+    }
 
-					if (items[i].name != "ColdPlay concert passes") {
-						if (items[i].quality > 0) {
-							if (items[i].name != "PokemonGo") {
-								items[i].quality = items[i].quality - 1;
-							}
-						}
-					} else {
-						// For Backstage passes with sellin less than zero
-						// quality is set to zero
-						items[i].quality = 0;
-					}
-				} else {
-					// For Aged Wine below 50 quality increases actually by 2
-					// In the previous line
-					if (items[i].quality < 50) {
-						items[i].quality = items[i].quality + 1;
-					}
-				}
-			}
-		}
-	}
+    private void decreaseQuality(Item item) {
+        if (item.quality > 0) {
+            item.quality--;
+        }
+    }
 
-	@Override
-	public String toString() {
-		return Arrays.toString(items);
-	}
+    private void decreaseSellIn(Item item) {
+        item.sellIn--;
+    }
 
+    @Override
+    public String toString() {
+        return Arrays.toString(items);
+    }
 }
